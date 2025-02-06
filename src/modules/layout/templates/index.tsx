4715b4c8d95d953c27a71e59e5ac724da181560d
mselbekk11 <mselbekk11@gmail.com>
2025-02-06 01:16:20 -0800

    updates

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
+import Hero from "@modules/home/components/hero"
 
 const Layout: React.FC<{
   children: React.ReactNode
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -13,7 +13,7 @@ export default async function Nav() {
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
       <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
-        <nav className="max-w-8xl content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+        <nav className="px-4 txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
               <SideMenu regions={regions} />
@@ -32,13 +32,13 @@ export default async function Nav() {
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
             <div className="hidden small:flex items-center gap-x-6 h-full">
-              <LocalizedClientLink
+              {/* <LocalizedClientLink
                 className="hover:text-ui-fg-base"
                 href="/account"
                 data-testid="nav-account-link"
               >
                 Account
-              </LocalizedClientLink>
+              </LocalizedClientLink> */}
             </div>
             <Suspense
               fallback={
